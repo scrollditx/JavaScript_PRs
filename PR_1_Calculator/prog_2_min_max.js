636645ab792c77e3e17d73ef444df277de2decea
scrollditx <parekhveddharmeshbhai@gmail.com>
2025-07-02 09:22:53 +0530

    mix-max done

--- PR_1_Calculator/prog_2_min_max.js
+++ PR_1_Calculator/prog_2_min_max.js
@@ -5,7 +5,7 @@ let num_1 = 10, // Change num_1, num_2, num_3
 console.log("--- Welcome to Minimum Maximum Finder ---");
 
 // 1. Basic Comparison
-console.log("\n--- Scenario-1: Basic Comparison ---");
+console.log("\n--- Scenario-1: Basic Comparison for Maximum ---");
 
 if (num_1 >= num_2) {
   console.log(`'${num_1}' is greater or equal to '${num_2}'`);
@@ -13,8 +13,16 @@ if (num_1 >= num_2) {
   console.log(`'${num_2}' is greater or equal to '${num_1}'`);
 }
 
+console.log("\n--- Scenario-2: Basic Comparison for Minimum ---");
+
+if (num_1 <= num_2) {
+  console.log(`'${num_1}' is lesser or equal to '${num_2}'`);
+} else {
+  console.log(`'${num_2}' is lesser or equal to '${num_1}'`);
+}
+
 // 2. Comparing '3' numbers
-console.log("\n--- Scenario-2: Comparing '3'  Numbers ---");
+console.log("\n--- Scenario-3: Comparing '3'  Numbers for Maximum---");
 
 if (num_1 >= num_2 && num_1 >= num_3) {
   console.log(`'${num_1}' is greater or equal to '${num_2}' and '${num_3}'`);
@@ -22,4 +30,14 @@ if (num_1 >= num_2 && num_1 >= num_3) {
   console.log(`'${num_2}' is greater or equal to '${num_1}' and '${num_3}'`);
 } else {
   console.log(`'${num_3}' is greater or equal to '${num_1}' and '${num_2}'`);
+}
+
+console.log("\n--- Scenario-3: Comparing '3'  Numbers for Minimum---");
+
+if (num_1 <= num_2 && num_1 <= num_3) {
+  console.log(`'${num_1}' is lesser or equal to '${num_2}' and '${num_3}'`);
+} else if (num_2 <= num_1 && num_2 <= num_3) {
+  console.log(`'${num_2}' is lesser or equal to '${num_1}' and '${num_3}'`);
+} else {
+  console.log(`'${num_3}' is lesser or equal to '${num_1}' and '${num_2}'`);
 }
